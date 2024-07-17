677f2431f54196cb95a021199f107e190764bafa
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-07-17 09:50:44 -0300

    feat(last commit): create index and server
    (do not checkout previus commit, use this one to refactor later)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,22 @@
 import { createServer } from "./server";
 import { log } from "@repo/logger";
+import http from "http";
+import { wss } from "./websocket";
 
 const port = process.env.PORT || 3001;
-const server = createServer();
+const app = createServer();
+const server = http.createServer(app);
+
+server.on('upgrade', (request, socket, head) => {
+  console.log('Handling upgrade request');
+  log('Handling upgrade request');
+  
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
 
 server.listen(port, () => {
   log(`api running on ${port}`);
+  console.log(`API running on port ${port}`);
 });
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,18 +2,20 @@ import { json, urlencoded } from "body-parser";
 import express, { type Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { testConnections } from "./services/prisma-service";
+import paymentRouter from "./routes/payment-route";
 
 export const createServer = (): Express => {
   const app = express();
+  testConnections();
+
   app
     .disable("x-powered-by")
     .use(morgan("dev"))
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
+    .use("/payments", paymentRouter)
     .get("/status", (_, res) => {
       return res.json({ ok: true });
     });
